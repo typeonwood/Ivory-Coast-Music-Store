6250b1bdfd4c09079e6b6886e22b748784529563
typeonwood <typeonwood@gmail.com>
2023-06-19 21:50:49 -0700

    updated tests

--- IvoryCoast/store/tests.py
+++ IvoryCoast/store/tests.py
@@ -20,10 +20,17 @@ class StoreViewTest(APITestCase):
     def setUp(self):
         self.item = StoreItem.objects.create(title='beluga', price=1000, description='woah this is cool', inventory=12)
 
+    def test_get_item_list(self):
+        response = self.client.get(reverse('store-list'))
+        item_list = StoreItem.objects.all()
+        serialized_list = StoreSerializer(item_list, many=True)
+        self.assertEqual(response.data, serialized_list.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
     def test_get_valid_single_item(self):
         response = self.client.get(reverse('store-detail', kwargs={'pk': self.item.pk}))
         store_item = StoreItem.objects.get(pk=self.item.pk)
-        serialized_item = StoreSerializer(store_item)
+        serialized_item = StoreSerializer(store_item, many=False)
         self.assertEqual(response.data, serialized_item.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
